ec11e7b300e09931380e86f29ae92fd4deb81d93
emmaramon05 <emrape@alumnatflorida.es>
2026-05-05 12:39:05 +0200

    Edición Escenas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if 
+    }
+
+    void PantallaJuego()
+    {
+
     }
 }
