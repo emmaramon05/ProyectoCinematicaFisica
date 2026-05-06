f96ac834f0bbda9f5d1ed0ae205c1b696b369630
emmaramon05 <emrape@alumnatflorida.es>
2026-05-06 17:26:58 +0200

    Pelota Movimiento

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BallScript : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    public float fuerza = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * fuerza, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * fuerza, ForceMode.Force);
+        }
     }
 }
